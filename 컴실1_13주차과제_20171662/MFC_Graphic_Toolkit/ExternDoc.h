ec1880e51d1d058051d5f5fb3001eaa6c2f0d001
Na Yeon Lee <sally0212@naver.com>
2019-02-07 03:30:22 +0900

    unnecessary folders

--- 컴실1_13주차과제_20171662/MFC_Graphic_Toolkit/ExternDoc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EXTERN_DOC_H__
-#define __EXTERN_DOC_H__
-
-#include "StdAfx.h"
-
-extern void setDocument(CDocument* pDoc);
-extern void setModifiedFlag(bool modified);
-
-#endif	// __EXTERN_DOC_H__
